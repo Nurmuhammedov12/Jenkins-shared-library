065f58079facee2480df3a84ab0263478b11909a
Nurmuhammedov12 <nurmuhammedowyhlas8@gmail.com>
2026-09-17 18:53:43 +0200

    test

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,6 @@
 #!/user/bin/env groovy
 import com.example.Docker
-def call(String imageName, String location) {
+def call(String imageName, String location){
     return new Docker(this).buildDockerImage(imageName, location)
 }
 
