ef995dda8fdea408666a482ca16b3a39de4a7449
Yhlas Nurmuhammedov <154041709+Nurmuhammedov12@users.noreply.github.com>
2026-09-17 17:49:03 +0200

    Refactor buildImage function to accept parameters

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/user/bin/env groovy
 
-def call() {
-    echo "building the docker image for Frontend"
+def call(String imageName, String location) {
+    echo "building the docker image for $location"
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh 'docker build -t nurmuhammedowyhlas/todo-app:jma-1.1 ./frontend'
+        sh "docker build -t $imageName ./$location"
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nurmuhammedowyhlas/todo-app:jma-1.0'
+        sh "docker push $imageName"
     }
 }
