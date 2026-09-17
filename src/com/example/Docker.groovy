aa73be89f730c2bec59e6d963db9dfe1a9252b33
Nurmuhammedov12 <nurmuhammedowyhlas8@gmail.com>
2026-09-17 18:22:45 +0200

    new functions
    # Conflicts:
    #	vars/buildImage.groovy

---
+++ src/com/example/Docker.groovy
@@ -0,0 +1,29 @@
+#!/user/bin/env groovy
+package com.example
+
+class Docker implements Serializable{
+    def script
+
+    Docker(script){
+        this.script = script;
+    }
+
+    def buildDockerImage(String imageName, String location){
+        script.echo "building the docker image for $location"
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+            script.sh "docker build -t $imageName ./$location"
+
+        }
+    }
+
+    def dockerLogin(){
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
+        }
+    }
+
+    def dockerPush(String imageName){
+        script.sh "docker push $imageName"
+    }
+}
+
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,6 @@
 #!/user/bin/env groovy
-
+import com.example.Docker
 def call(String imageName, String location) {
-    echo "building the docker image for $location"
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh "docker build -t $imageName ./$location"
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh "docker push $imageName"
-    }
+    return new Docker(this).buildDockerImage(imageName, location)
 }
+
